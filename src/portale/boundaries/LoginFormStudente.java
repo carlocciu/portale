4aa237aa4eefa62ae71fa6e59ea8ad9c2be7199d
Riccardo Musmeci <musmex92@gmail.com>
2016-02-20 09:31:18 +0100

    Aggiunte le due possibilità di login: Studente e Docente con due pagine separate. [IMPORTANTE] Quando bisogna provare l'applicazione per uno dei due profili, bisogna andare a modificare in Main.java il parametro di blbl.getResource("../res/LoginForm[Studente|Docente]".fxml")

---
+++ src/portale/boundaries/LoginFormStudente.java
@@ -0,0 +1,24 @@
+package portale.boundaries;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import portale.controls.AutenticazioneDocenteCtrl;
+
+public class LoginFormStudente {
+
+    @FXML private TextField matricolaTF;
+
+    @FXML private PasswordField passPF;
+
+    @FXML private Button loginButton;
+
+    @FXML public void clickLogin() throws Exception{
+
+
+
+    }
+
+}
