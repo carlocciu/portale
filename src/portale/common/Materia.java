ce5397ada684745924faac887d2e9e4b4a0e66b6
carlonuccio <carlonuccio91@gmail.com>
2016-02-22 22:08:06 +0100

    Commenti

--- src/portale/common/Materia.java
+++ src/portale/common/Materia.java
@@ -1,25 +1,58 @@
 package portale.common;
 
+/**
+ * Classe contenente le informazioni relative ad una materia
+ */
 public class Materia {
-
+    /**
+     * Nome materia
+     */
     private String nomeMateria;
 
+    /**
+     * Codice materia
+     */
     private String codiceMateria;
 
+    /**
+     * Ordinamento (vecchio/nuovo)
+     */
     private String ordinamento;
 
+    /**
+     * Informazioni del docente che sostiene la materia
+     */
     private DocenteClass mDocente;
 
+    /**
+     * CFU
+     */
     private int CFU;
 
+    /**
+     * Anno in cui si svolgono le lezioni della materia
+     */
     private int annoCorso;
 
+    /**
+     * Genera un oggetto Materia
+     * @param codiceMateria codice materia
+     * @param nomeMateria nome materia
+     * @param CFU cfu
+     */
     public Materia(String codiceMateria, String nomeMateria, int CFU) {
         this.codiceMateria = codiceMateria;
         this.nomeMateria = nomeMateria;
         this.CFU = CFU;
     }
 
+    /**
+     * Genera un oggetto Materia
+     * @param codiceMateria codice materia
+     * @param nomeMateria nome materia
+     * @param CFU cfu
+     * @param anno anno del corso
+     */
     public Materia(String codiceMateria, String nomeMateria, int CFU, int anno) {
         this.codiceMateria = codiceMateria;
         this.nomeMateria = nomeMateria;
@@ -27,6 +60,14 @@ public class Materia {
         this.annoCorso = anno;
     }
 
+    /**
+     * Genera un oggetto Materia
+     * @param codiceMateria codice materia
+     * @param nomeMateria nome materia
+     * @param CFU cfu
+     * @param pOrdinamento ordinamento
+     * @param anno anno del corso
+     */
     public Materia(String codiceMateria, String nomeMateria, String pOrdinamento, int CFU, int anno) {
         this.codiceMateria = codiceMateria;
         this.nomeMateria = nomeMateria;
@@ -35,6 +76,15 @@ public class Materia {
         this.annoCorso = anno;
     }
 
+    /**
+     * Genera un oggetto Materia
+     * @param codiceMateria codice materia
+     * @param nomeMateria nome materia
+     * @param mDocente informazioni docente
+     * @param CFU cfu
+     * @param ordinamento ordinamento
+     * @param annoCorso  anno del corso
+     */
     public Materia(String nomeMateria, String codiceMateria, DocenteClass mDocente, String ordinamento, int CFU, int annoCorso) {
         this.nomeMateria = nomeMateria;
         this.codiceMateria = codiceMateria;
@@ -44,54 +94,106 @@ public class Materia {
         this.annoCorso = annoCorso;
     }
 
+    /**
+     * Ritorna il nome della materia
+     * @return nome materia
+     */
     public String getNomeMateria() {
         return nomeMateria;
     }
 
+    /**
+     * Setta il nome della materia
+     * @param nomeMateria nome materia
+     */
     public void setNomeMateria(String nomeMateria) {
         this.nomeMateria = nomeMateria;
     }
 
+    /**
+     * Ritorna il codice della materia
+     * @return codice materia
+     */
     public String getCodiceMateria() {
         return codiceMateria;
     }
 
+    /**
+     * Setta il codice della materia
+     * @param codiceMateria codice materia
+     */
     public void setCodiceMateria(String codiceMateria) {
         this.codiceMateria = codiceMateria;
     }
 
+    /**
+     * Ritorna l'ordinamento della materia
+     * @return ordinamento
+     */
     public String getOrdinamento() {
         return ordinamento;
     }
 
+    /**
+     * Setta l'ordinamento della materia
+     * @param ordinamento ordinamento
+     */
     public void setOrdinamento(String ordinamento) {
         this.ordinamento = ordinamento;
     }
 
+    /**
+     * Ritorna le informazioni del docente che sostiene la materia
+     * @return informazioni docente
+     */
     public DocenteClass getmDocente() {
         return mDocente;
     }
 
+    /**
+     * Setta le informazioni del docente
+     * @param mDocente informazioni docente
+     */
     public void setmDocente(DocenteClass mDocente) {
         this.mDocente = mDocente;
     }
 
+    /**
+     * Ritorna i CFU della materia
+     * @return cfu
+     */
     public int getCFU() {
         return CFU;
     }
 
+    /**
+     * Setta i cfu della materia
+     * @param CFU cfu
+     */
     public void setCFU(int CFU) {
         this.CFU = CFU;
     }
 
+    /**
+     * Ritorna l'anno della materia
+     * @return anno corso della materia
+     */
     public int getAnnoCorso() {
         return annoCorso;
     }
 
+    /**
+     * Setta l'anno della materia
+     * @param annoCorso anno materia
+     */
     public void setAnnoCorso(int annoCorso) {
         this.annoCorso = annoCorso;
     }
 
+    /**
+     * Override del metodo toString() per la classe Materia
+     * @return una stringa contenente codice e nome della materia
+     */
     @Override
     public String toString() {
         return codiceMateria + " - " + nomeMateria + " - " + CFU + " CFU";
--- src/portale/common/StudenteClass.java
+++ src/portale/common/StudenteClass.java
@@ -1,17 +1,41 @@
 package portale.common;
 
+/**
+ * Classe contenente le informazioni relative ad uno studente
+ */
 public class StudenteClass {
 
+    /**
+     * Nome studente
+     */
     private String nomeStudente;
 
+    /**
+     * Cognome studente
+     */
     private String cognomeStudente;
 
+    /**
+     * Matricola studente
+     */
     private String matricolaStudente;
 
+    /**
+     * Indirizzo e-mail studente
+     */
     private String indirizzoEmail;
 
+    /**
+     * Recapito telefonico dello studente
+     */
     private String recapito;
 
+    /**
+     * Genera un oggetto StudenteClass
+     * @param nomeStudente nome studente
+     * @param cognomeStudente cognome studente
+     * @param matricolaStudente matricola studente
+     */
     public StudenteClass(String nomeStudente, String cognomeStudente, String matricolaStudente) {
         this.nomeStudente = nomeStudente;
         this.cognomeStudente = cognomeStudente;
@@ -19,46 +43,90 @@ public class StudenteClass {
 
     }
 
+    /**
+     * Ritorna il nome dello studente
+     * @return nome studente
+     */
     public String getNomeStudente() {
         return nomeStudente;
     }
 
+    /**
+     * Setta il nome dello studente
+     * @param nomeStudente nome studente
+     */
     public void setNomeStudente(String nomeStudente) {
         this.nomeStudente = nomeStudente;
     }
 
+    /**
+     * Ritorna il cognome dello studente
+     * @return cognome studente
+     */
     public String getCognomeStudente() {
         return cognomeStudente;
     }
 
+    /**
+     * Setta il cognome dello studente
+     * @param cognomeStudente cognome studente
+     */
     public void setCognomeStudente(String cognomeStudente) {
         this.cognomeStudente = cognomeStudente;
     }
 
+    /**
+     * Ritorna la matricola dello studente
+     * @return matricola studente
+     */
     public String getMatricolaStudente() {
         return matricolaStudente;
     }
 
+    /**
+     * Setta la matricola dello studente
+     * @param matricolaStudente matricola studente
+     */
     public void setMatricolaStudente(String matricolaStudente) {
         this.matricolaStudente = matricolaStudente;
     }
 
+    /**
+     * Ritorna l'indirizzo e-mail dello studente
+     * @return indirizzo e-mail studente
+     */
     public String getIndirizzoEmail() {
         return indirizzoEmail;
     }
 
+    /**
+     * Setta l'indirizzo e-mail dello studente
+     * @param indirizzoEmail indirizzo e-mail
+     */
     public void setIndirizzoEmail(String indirizzoEmail) {
         this.indirizzoEmail = indirizzoEmail;
     }
 
+    /**
+     * Ritorna il recapito telefonico dello studente
+     * @return recapito
+     */
     public String getRecapito() {
         return recapito;
     }
 
+    /**
+     * Setta il recapito telefonico dello studente
+     * @param recapito recapito
+     */
     public void setRecapito(String recapito) {
         this.recapito = recapito;
     }
 
+    /**
+     * Override del metodo toString() per la classe StudenteClass
+     * @return una stringa contenente matricola, nome e cognome dello studente
+     */
     @Override
     public String toString() {
         return matricolaStudente + " - " + nomeStudente + " " + cognomeStudente;
